2b94d303b461efb47087157587147c47e0c95388
Vitor Petri <laabenne@gmail.com>
2021-06-16 20:01:25 -0300

    first commit

---
+++ js/vanta.js
@@ -0,0 +1,14 @@
+VANTA.FOG({
+    el: "#vanta-canvas",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    highlightColor: 0xdca327,
+    midtoneColor: 0xa2914d,
+    lowlightColor: 0xbbad58,
+    baseColor: 0xaaaaaa,
+    blurFactor: 0.69,
+    zoom: 0.50
+})
